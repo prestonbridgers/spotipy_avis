67cbc1df6b119afea231fb23543cf95bb678e4d2
Curt Bridgers <bridgerspc@gmail.com>
2023-04-12 12:18:37 -0400

    Added function to recursively look at all .h5 files in the million song subset. Added the artist and track names of all songs in the subset in song_list.txt

--- hdf_songs.py
+++ hdf_songs.py
@@ -1,5 +1,6 @@
 import os
 import h5py
+import pathlib
 
 
 class HDFViewer:
@@ -11,17 +12,16 @@ class HDFViewer:
         with h5py.File(song_path, "r") as f:
             metadata = list(f.keys())[1]
 
-            print('Artist:', f[metadata]['songs']['artist_name'][0].decode('ascii'))
-            print('Track:', f[metadata]['songs']['title'][0].decode('ascii'))
+            print('Artist:', f[metadata]['songs']['artist_name'][0].decode('utf-8'))
+            print('Track:', f[metadata]['songs']['title'][0].decode('utf-8'))
 
 
 def main():
-    song_dir = '/home/bridgerspc/Code/spotipy_avis/MillionSongSubset/data/A/A/A/'
-    songs = os.listdir(song_dir)
+    song_dir = pathlib.Path('/home/bridgerspc/Code/spotipy_avis/MillionSongSubset/data/')
+    songs = song_dir.rglob('*.h5')
     song_list = []
     for song in songs:
         song_list.append(os.path.join(song_dir, song))
-    print(song_list)
     count = 0
     for song in song_list:
         print(f'~~~~~~~~~~~~~~~~~~~~~~~~~~~Track #{count}~~~~~~~~~~~~~~~~~~~~~~~~~~~')
