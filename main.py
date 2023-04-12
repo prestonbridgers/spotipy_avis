a9405f2c8af9e93bdfe00ee47cab557d03b346f6
Curt Bridgers <bridgerspc@gmail.com>
2023-04-12 11:06:02 -0400

    Removed unnecessary calculate function

--- main.py
+++ main.py
@@ -9,21 +9,23 @@ class AVisGUI():
         self.root = Tk()
         self.root.title("Spotipy AVis")
         self.root.grid_rowconfigure(0, weight=1)
-        self.root.grid_rowconfigure(1, weight=1)
+        self.root.grid_rowconfigure(1, weight=5)
+        self.root.grid_rowconfigure(2, weight=1)
         self.root.grid_columnconfigure(0, weight=1)
-        self.root.grid_columnconfigure(1, weight=1)
 
         self.mainframe = ttk.Frame(self.root,
                                    width=700,
-                                   height=500)
+                                   height=500,
+                                   borderwidth=5)
+        self.mainframe.grid_rowconfigure(0, weight=1)
+        self.mainframe.grid_columnconfigure(0, weight=1)
+        self.mainframe.grid_columnconfigure(1, weight=1)
         self.mainframe.grid(column=0,
                             row=0)
 
         self.selected_audiofile = StringVar()
         self.audio_selector = ttk.Combobox(self.mainframe,
-                                           textvariable=self.selected_audiofile,
-                                           width=100,
-                                           height=25)
+                                           textvariable=self.selected_audiofile)
         self.audio_selector['values'] = AVisGUI.get_audio_files()
         self.audio_selector.grid(column=1, row=0)
 
@@ -31,19 +33,22 @@ class AVisGUI():
         self.audio_selector_label.grid(column=0, row=0)
 
         self.visual_canvas = Canvas(self.root, width=700, height=400, background='gray75')
-        self.visual_canvas.grid(column=0, row=1, sticky="N S E W")
+        self.visual_canvas.grid(column=0, row=1, sticky='n s e w')
 
-        self.controls_frame = ttk.Frame(self.root)
-        self.controls_frame.grid(column=0, row=2, sticky="N S W E")
+        self.controls_frame = ttk.Frame(self.root, borderwidth=5)
+        self.controls_frame.grid(column=0, row=2)
+        self.controls_frame.grid_rowconfigure(0, weight=1)
+        self.controls_frame.grid_columnconfigure(0, weight=1)
+        self.controls_frame.grid_columnconfigure(1, weight=1)
 
         self.play_button = ttk.Button(self.controls_frame, text="Play", command=self.on_play)
-        self.play_button.grid(column=0, row=0, sticky="W")
+        self.play_button.grid(column=0, row=0)
 
         self.pause_button = ttk.Button(self.controls_frame, text="Pause", command=self.on_pause)
         self.pause_button.grid(column=1, row=0)
 
         self.stop_button = ttk.Button(self.controls_frame, text="Stop", command=self.on_stop)
-        self.stop_button.grid(column=2, row=0, sticky="E")
+        self.stop_button.grid(column=2, row=0)
 
     @staticmethod
     def get_audio_files():
@@ -57,13 +62,6 @@ class AVisGUI():
 
     def on_stop(self):
         print('User has stopped')
-
-    def calculate(self, *args):
-        try:
-            value = float(self.feet.get())
-            self.meters.set(str(value))
-        except ValueError:
-            pass
 
     def start(self):
         self.root.mainloop()
