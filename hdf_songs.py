aa02e635973072ecc9919d6bdf41ee1e20a234ee
Curt Bridgers <bridgerspc@gmail.com>
2023-04-17 13:02:47 -0400

    Added csv output instead of normal output for the million song subset csv file.

--- hdf_songs.py
+++ hdf_songs.py
@@ -12,19 +12,19 @@ class HDFViewer:
         with h5py.File(song_path, "r") as f:
             metadata = list(f.keys())[1]
 
-            print('Artist:', f[metadata]['songs']['artist_name'][0].decode('utf-8'))
-            print('Track:', f[metadata]['songs']['title'][0].decode('utf-8'))
+            print(f[metadata]['songs']['artist_name'][0].decode('utf-8'), end=',')
+            print(f[metadata]['songs']['title'][0].decode('utf-8'))
 
 
 def main():
-    song_dir = pathlib.Path('/home/bridgerspc/Code/spotipy_avis/MillionSongSubset/data/')
+    song_dir = pathlib.Path(os.path.abspath('./MillionSongSubset/data/'))
     songs = song_dir.rglob('*.h5')
     song_list = []
     for song in songs:
         song_list.append(os.path.join(song_dir, song))
     count = 0
     for song in song_list:
-        print(f'~~~~~~~~~~~~~~~~~~~~~~~~~~~Track #{count}~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print(f'{song}', end=',')
         HDFViewer.view_data(song)
         count += 1
 
