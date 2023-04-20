ad6bfa33ac3f79f36970fe3a4f48e3254ce452d1
Curt Bridgers <bridgerspc@gmail.com>
2023-04-20 10:25:02 -0400

    Restructured things and split the src code into separate files for ease of maintenance.

--- src/avisgui.py
+++ src/avisgui.py
@@ -1,49 +1,8 @@
-import multiprocessing
-
-import pyaudio
 import os
-import wave
-import pyaudio
-import time
-from multiprocessing import Process
 from tkinter import *
 from tkinter import ttk
 
-
-class MusicPlayer:
-    def __init__(self):
-        multiprocessing.set_start_method('spawn')
-        self.proc = None
-
-    def play_song(self, audio_file):
-        self.proc = Process(target=self.music_process_play_song, args=(audio_file,))
-        self.proc.start()
-
-    def terminate(self):
-        self.stop_song()
-
-    def stop_song(self):
-        self.proc.terminate()
-
-    def music_process_play_song(self, audio_file):
-        audio_path = os.path.abspath(os.path.join('./audio', audio_file))
-        print(f'Playing: {audio_path}')
-
-        p = pyaudio.PyAudio()
-
-        with wave.open(audio_path, 'rb') as wf:
-            def callback(in_data, frame_count, time_info, status):
-                data = wf.readframes(frame_count)
-                return data, pyaudio.paContinue
-
-            audio_stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                                  channels=wf.getnchannels(),
-                                  rate=wf.getframerate(),
-                                  output=True,
-                                  stream_callback=callback)
-
-            while audio_stream.is_active():
-                time.sleep(1)
+from music_player import MusicPlayer
 
 
 class AVisGUI:
@@ -95,27 +54,26 @@ class AVisGUI:
 
     @staticmethod
     def get_audio_files():
-        return os.listdir("./audio")
+        return os.listdir("../audio")
 
     def on_play(self):
         file = self.selected_audiofile.get()
         self.mp.play_song(file)
 
     def on_pause(self):
-        print('User has paused')
+        self.mp.stop_song()
 
     def on_stop(self):
         self.mp.stop_song()
 
     def start(self):
         self.root.mainloop()
-
+        self.mp.terminate()
 
 
 def main():
     app = AVisGUI()
     app.start()
-    app.mp.terminate()
 
 
 if __name__ == "__main__":
---
+++ src/music_player.py
@@ -0,0 +1,43 @@
+import multiprocessing
+from multiprocessing import Process
+import os
+import pyaudio
+import time
+import wave
+
+
+class MusicPlayer:
+    def __init__(self):
+        multiprocessing.set_start_method('spawn')
+        self.proc = None
+
+    def play_song(self, audio_file):
+        self.proc = Process(target=MusicPlayer.music_process_play_song, args=(audio_file,))
+        self.proc.start()
+
+    def terminate(self):
+        self.stop_song()
+
+    def stop_song(self):
+        self.proc.terminate()
+
+    @staticmethod
+    def music_process_play_song(audio_file):
+        audio_path = os.path.abspath(os.path.join('../audio', audio_file))
+        print(f'Playing: {audio_path}')
+
+        p = pyaudio.PyAudio()
+
+        with wave.open(audio_path, 'rb') as wf:
+            def callback(in_data, frame_count, time_info, status):
+                data = wf.readframes(frame_count)
+                return data, pyaudio.paContinue
+
+            audio_stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                                  channels=wf.getnchannels(),
+                                  rate=wf.getframerate(),
+                                  output=True,
+                                  stream_callback=callback)
+
+            while audio_stream.is_active():
+                time.sleep(1)
