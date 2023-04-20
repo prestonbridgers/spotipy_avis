652679d47e4b4894586268bc3a92bbd199eecb48
Curt Bridgers <bridgerspc@gmail.com>
2023-04-20 10:18:52 -0400

    The Audio now plays! NOTE: Need to use system python (IE /usr/bin/python3) instead of conda. Conda's packaged pyaudio has a bug in it :/

--- main.py
+++ main.py
@@ -1,11 +1,54 @@
+import multiprocessing
+
 import pyaudio
 import os
+import wave
+import pyaudio
+import time
+from multiprocessing import Process
 from tkinter import *
 from tkinter import ttk
 
 
-class AVisGUI():
+class MusicPlayer:
     def __init__(self):
+        multiprocessing.set_start_method('spawn')
+        self.proc = None
+
+    def play_song(self, audio_file):
+        self.proc = Process(target=self.music_process_play_song, args=(audio_file,))
+        self.proc.start()
+
+    def terminate(self):
+        self.stop_song()
+
+    def stop_song(self):
+        self.proc.terminate()
+
+    def music_process_play_song(self, audio_file):
+        audio_path = os.path.abspath(os.path.join('./audio', audio_file))
+        print(f'Playing: {audio_path}')
+
+        p = pyaudio.PyAudio()
+
+        with wave.open(audio_path, 'rb') as wf:
+            def callback(in_data, frame_count, time_info, status):
+                data = wf.readframes(frame_count)
+                return data, pyaudio.paContinue
+
+            audio_stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                                  channels=wf.getnchannels(),
+                                  rate=wf.getframerate(),
+                                  output=True,
+                                  stream_callback=callback)
+
+            while audio_stream.is_active():
+                time.sleep(1)
+
+
+class AVisGUI:
+    def __init__(self):
+        self.mp = MusicPlayer()
         self.root = Tk()
         self.root.title("Spotify AVis")
         self.root.grid_rowconfigure(0, weight=1)
@@ -55,21 +98,24 @@ class AVisGUI():
         return os.listdir("./audio")
 
     def on_play(self):
-        print(f'User has played {self.selected_audiofile.get()}')
+        file = self.selected_audiofile.get()
+        self.mp.play_song(file)
 
     def on_pause(self):
         print('User has paused')
 
     def on_stop(self):
-        print('User has stopped')
+        self.mp.stop_song()
 
     def start(self):
         self.root.mainloop()
 
 
+
 def main():
     app = AVisGUI()
     app.start()
+    app.mp.terminate()
 
 
 if __name__ == "__main__":
